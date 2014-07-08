ecd845be17703e9ed6848a846bd037b614cd356a
eeue56 <eeue56@gmail.com>
2014-07-08 09:40:41 +0100

    all the hacks

--- client/scripts/services/db.js
+++ client/scripts/services/db.js
@@ -1,3 +1,25 @@
 module.exports = function() {
-  return new Firebase('https://astralchai.firebaseio.com');
+  //return new Firebase('https://astralchai.firebaseio.com');
+  return {
+    child : function(){
+      console.log('child called');
+      return {
+        child : function(){
+          console.log('from db called');
+          return {
+            on : function(durp, cb){
+              cb({
+                val : function(){
+                  return true;
+                }
+              });
+              console.log('on called');
+              return;
+            }
+          }
+        }
+      }
+    },
+    push : function(){}
+  }; 
 };
